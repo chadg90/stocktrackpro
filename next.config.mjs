170da915949de8d619b765978f90158c4391b704
chadg90 <163060226+chadg90@users.noreply.github.com>
2025-02-04 21:40:46 +0000

    Update Next.js config for Tailwind

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['postcss-loader'],
+    });
+    return config;
+  },
   output: 'export',
   images: {
     unoptimized: true,
